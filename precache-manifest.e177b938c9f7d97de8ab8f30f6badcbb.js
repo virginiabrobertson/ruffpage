a21c3ace3d4fb4a402a30b35bc4e62e431807718
Virginia B Robertson <virginiabrobertson@yahoo.com>
2022-08-12 15:00:02 -0400

    Updates

---
+++ precache-manifest.e177b938c9f7d97de8ab8f30f6badcbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d0865d42dee4c8c40479",
+    "url": "/ruffpage/static/css/main.9b8e4512.chunk.css"
+  },
+  {
+    "revision": "d0865d42dee4c8c40479",
+    "url": "/ruffpage/static/js/main.d0865d42.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/ruffpage/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/ruffpage/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "d157e3278f45ff0f495b",
+    "url": "/ruffpage/static/js/runtime~main.d157e327.js"
+  },
+  {
+    "revision": "027138053f05607fb1047841f91cc76f",
+    "url": "/ruffpage/static/media/logo.02713805.jpg"
+  },
+  {
+    "revision": "9ecfcbbd5fd6cac184a89b7b0dfdf702",
+    "url": "/ruffpage/index.html"
+  }
+];
